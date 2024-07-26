2d173ab20da21b8f8eb5186e5e1f5b592625fb13
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-26 16:33:23 -0300

    coder Nextjs: setando o projeto

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        // process.env.BDpath
+        BDpath: "BDpath..."
+    }
+};
 
 export default nextConfig;
